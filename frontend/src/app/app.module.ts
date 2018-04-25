4a96bf1f5bebd09da2f9cb307ee1549a0f354cd0
Bence Lovas <bansoul1@gmail.com>
2018-04-25 17:34:54 +0200

    @front - registration-form created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LoginFormComponent } from './login/login-form/login-form.component';
+import { RegistrationFormComponent } from './login/registration-form/registration-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    RegistrationFormComponent
   ],
   imports: [
     BrowserModule
