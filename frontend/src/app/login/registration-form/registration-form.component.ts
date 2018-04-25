6ac023a05dab821ae65d215a37aef113db8c038d
Bence Lovas <bansoul1@gmail.com>
2018-04-25 19:30:30 +0200

    @front - form validation added to registration form's class

--- frontend/src/app/login/registration-form/registration-form.component.ts
+++ frontend/src/app/login/registration-form/registration-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registration-form',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationFormComponent implements OnInit {
 
-  constructor() { }
+  registrationForm: FormGroup;
+  matcher = {
+    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+      const isSubmitted = form && form.submitted;
+      return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    }
+  };
+
+  constructor(
+    private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      'username': ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(30),
+      ])],
+      'password': ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20),
+      ])],
+      'email': ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
+      ])],
+    });
+  }
+
+  onSubmit(form: any) {
+    console.log(form);
   }
 
 }
