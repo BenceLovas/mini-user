0c4d9ec05ac947cb0c30c24e2fa8a6f0d1f7e5cd
Bence Lovas <bansoul1@gmail.com>
2018-04-28 14:03:14 +0200

    @back - user controller, service and repository classes created

---
+++ backend/src/main/java/com/bencelovas/user/UserController.java
@@ -0,0 +1,18 @@
+package com.bencelovas.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+}
