2e6e96c6dc6b4a7c5c3caddda5f2917738aef53f
Bence Lovas <bansoul1@gmail.com>
2018-04-28 14:45:37 +0200

    @back - dummy endpoint created for user registration

--- backend/src/main/java/com/bencelovas/user/UserController.java
+++ backend/src/main/java/com/bencelovas/user/UserController.java
@@ -1,8 +1,13 @@
 package com.bencelovas.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api")
@@ -13,6 +18,14 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity registration(@RequestBody User userInput) {
+        System.out.println(userInput.getName());
+        System.out.println(userInput.getPassword());
+        System.out.println(userInput.getEmail());
+        return ResponseEntity.ok(Collections.singletonMap("response", "success"));
     }
 
 }
