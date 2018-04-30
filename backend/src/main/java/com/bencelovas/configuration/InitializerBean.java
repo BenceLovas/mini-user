fdb0123b3da12510c70c70ae55dc504360bb106b
Bence Lovas <bansoul1@gmail.com>
2018-04-30 20:23:20 +0200

    @back - database populated with random users on server start

--- backend/src/main/java/com/bencelovas/configuration/InitializerBean.java
+++ backend/src/main/java/com/bencelovas/configuration/InitializerBean.java
@@ -11,11 +11,36 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Component
 public class InitializerBean {
 
     private UserRepository userRepository;
+    private List<User> users = new ArrayList<User>() {{
+        add(new User("Pedro Michalek", BCrypt.hashpw("password", BCrypt.gensalt()), "pedro@gmail.com"));
+        add(new User("Michelle Runkle", BCrypt.hashpw("password", BCrypt.gensalt()), "michelle@gmail.com"));
+        add(new User("Eleanor Canedy", BCrypt.hashpw("password", BCrypt.gensalt()), "eleanor@gmail.com"));
+        add(new User("Georgia Likens", BCrypt.hashpw("password", BCrypt.gensalt()), "georgia@gmail.com"));
+        add(new User("Phil Jerman", BCrypt.hashpw("password", BCrypt.gensalt()), "phil@gmail.com"));
+        add(new User("Brigitte Hooten", BCrypt.hashpw("password", BCrypt.gensalt()), "brigitte@gmail.com"));
+        add(new User("Ricarda Chrisman", BCrypt.hashpw("password", BCrypt.gensalt()), "ricarda@gmail.com"));
+        add(new User("Keeley Drakeford", BCrypt.hashpw("password", BCrypt.gensalt()), "keeley@gmail.com"));
+        add(new User("Drew Baham", BCrypt.hashpw("password", BCrypt.gensalt()), "drew@gmail.com"));
+        add(new User("Rachel Dimond", BCrypt.hashpw("password", BCrypt.gensalt()), "rachel@gmail.com"));
+        add(new User("Quinton Mccrea", BCrypt.hashpw("password", BCrypt.gensalt()), "quinton@gmail.com"));
+        add(new User("Sharen Risser", BCrypt.hashpw("password", BCrypt.gensalt()), "sharen@gmail.com"));
+        add(new User("Theresia Hedgepeth", BCrypt.hashpw("password", BCrypt.gensalt()), "theresia@gmail.com"));
+        add(new User("Monnie Darville", BCrypt.hashpw("password", BCrypt.gensalt()), "monnie@gmail.com"));
+        add(new User("Dino Ocampo", BCrypt.hashpw("password", BCrypt.gensalt()), "dino@gmail.com"));
+        add(new User("Tina Pegram", BCrypt.hashpw("password", BCrypt.gensalt()), "tina@gmail.com"));
+        add(new User("Florance Abernethy", BCrypt.hashpw("password", BCrypt.gensalt()), "florance@gmail.com"));
+        add(new User("Elly Joe", BCrypt.hashpw("password", BCrypt.gensalt()), "elly@gmail.com"));
+        add(new User("Rhiannon Messerly", BCrypt.hashpw("password", BCrypt.gensalt()), "rhiannon@gmail.com"));
+        add(new User("Sheryll Overbey", BCrypt.hashpw("password", BCrypt.gensalt()), "sheryll@gmail.com"));
+    }};
 
     @Autowired
     public InitializerBean(UserRepository userRepository) {
@@ -35,7 +60,7 @@ public class InitializerBean {
 
     @PostConstruct
     public void populateDatabase() {
-        //TODO
+        userRepository.save(users);
     }
 
 }
