ec30d7b7fc2fb81e79e2e966d6a1d86f8b05d8a6
Bence Lovas <bansoul1@gmail.com>
2018-05-01 00:36:29 +0200

    @front - user list table created in admin dashboard and filled with data on page load

--- frontend/src/app/admin-dashboard/admin-dashboard.component.ts
+++ frontend/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {UserService} from '../user.service';
+import {User} from '../user';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns = ['id', 'name', 'email', 'actions'];
+  dataSource: MatTableDataSource<User>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
+    this.userService.getUsers()
+      .subscribe(
+        data => {
+          this.dataSource = new MatTableDataSource(data);
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+          this.dataSource.sortingDataAccessor = (userData, sortHeaderID) => {
+            this.paginator.pageIndex = 0;
+            if (typeof userData[sortHeaderID] === 'string') {
+              return userData[sortHeaderID].toLocaleLowerCase();
+            }
+            return userData[sortHeaderID];
+          };
+          this.dataSource.filterPredicate = (userData: User, filter: string): boolean => {
+            this.paginator.pageIndex = 0;
+            const reducedUserData = {'id': userData.id, 'name': userData.name, 'email': userData.email};
+            const accumulator = (currentTerm, key) => currentTerm + reducedUserData[key];
+            const userDataStr = Object.keys(userData).reduce(accumulator, '').toLowerCase();
+
+            const transformedFilter = filter.trim().toLowerCase();
+
+            return userDataStr.indexOf(transformedFilter) !== -1;
+          };
+        },
+        error => console.log(error)
+      );
   }
 
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
+
+  deleteUser(user) {
+    console.log(user);
+  }
 }
