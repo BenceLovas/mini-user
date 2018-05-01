f59317634384b5b5e81f1ed0353a8adf0d284254
Bence Lovas <bansoul1@gmail.com>
2018-05-01 06:57:15 +0200

    @front - routes guarded in routing module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import {LoginComponent} from './login/login.component';
+import {UserAuthenticationService} from './user-authentication.service';
+import {AdminAuthenticationService} from './admin-authentication.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'admin/dashboard', component: AdminDashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [UserAuthenticationService] },
+  { path: 'admin/dashboard', component: AdminDashboardComponent, canActivate: [AdminAuthenticationService] },
+  { path: '', canActivate: [UserAuthenticationService], redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
