1a0450a941640c1eddac02ccc9fb340bf0be3245
Bence Lovas <bansoul1@gmail.com>
2018-05-01 03:22:06 +0200

    @front - admin dashboard table's data filling logic implemented

--- frontend/src/app/admin-dashboard/admin-dashboard.component.ts
+++ frontend/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -12,6 +12,8 @@ export class AdminDashboardComponent implements OnInit {
 
   displayedColumns = ['id', 'name', 'email', 'actions'];
   dataSource: MatTableDataSource<User>;
+  filter = '';
+  afterDelete = false;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -21,28 +23,7 @@ export class AdminDashboardComponent implements OnInit {
   ngOnInit() {
     this.userService.getUsers()
       .subscribe(
-        data => {
-          this.dataSource = new MatTableDataSource(data);
-          this.dataSource.paginator = this.paginator;
-          this.dataSource.sort = this.sort;
-          this.dataSource.sortingDataAccessor = (userData, sortHeaderID) => {
-            this.paginator.pageIndex = 0;
-            if (typeof userData[sortHeaderID] === 'string') {
-              return userData[sortHeaderID].toLocaleLowerCase();
-            }
-            return userData[sortHeaderID];
-          };
-          this.dataSource.filterPredicate = (userData: User, filter: string): boolean => {
-            this.paginator.pageIndex = 0;
-            const reducedUserData = {'id': userData.id, 'name': userData.name, 'email': userData.email};
-            const accumulator = (currentTerm, key) => currentTerm + reducedUserData[key];
-            const userDataStr = Object.keys(userData).reduce(accumulator, '').toLowerCase();
-
-            const transformedFilter = filter.trim().toLowerCase();
-
-            return userDataStr.indexOf(transformedFilter) !== -1;
-          };
-        },
+        data => this.fillTable(data),
         error => console.log(error)
       );
   }
@@ -50,10 +31,51 @@ export class AdminDashboardComponent implements OnInit {
   applyFilter(filterValue: string) {
     filterValue = filterValue.trim();
     filterValue = filterValue.toLowerCase();
+    this.filter = filterValue;
     this.dataSource.filter = filterValue;
   }
 
   deleteUser(user) {
-    console.log(user);
+    this.userService.deleteUser(user)
+      .subscribe(
+        data => {
+          this.afterDelete = true;
+          this.fillTable(data);
+          this.afterDelete = false;
+        },
+        error => console.log(error),
+      );
   }
+
+  fillTable(data) {
+    this.dataSource = new MatTableDataSource(data);
+    this.dataSource.sort = this.sort;
+    this.dataSource.sortingDataAccessor = (userData, sortHeaderID) => {
+      if (!this.afterDelete) {
+        this.paginator.pageIndex = 0;
+      }
+      if (typeof userData[sortHeaderID] === 'string') {
+        return userData[sortHeaderID].toLocaleLowerCase();
+      }
+      return userData[sortHeaderID];
+    };
+    this.dataSource.filterPredicate = (userData: User, filter: string): boolean => {
+      if (!this.afterDelete) {
+        this.paginator.pageIndex = 0;
+      }
+      const reducedUserData = {'id': userData.id, 'name': userData.name, 'email': userData.email};
+      const accumulator = (currentTerm, key) => currentTerm + reducedUserData[key];
+      const userDataStr = Object.keys(userData).reduce(accumulator, '').toLowerCase();
+
+      const transformedFilter = filter.trim().toLowerCase();
+
+      return userDataStr.indexOf(transformedFilter) !== -1;
+    };
+    this.dataSource.paginator = this.paginator;
+    if (this.dataSource.paginator.pageSize * this.dataSource.paginator.pageIndex >= data.length) {
+      this.dataSource.paginator.previousPage();
+    }
+    this.applyFilter(this.filter);
+  }
+
 }
